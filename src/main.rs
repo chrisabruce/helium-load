de743155ee1265fe57dae956ad31c9e8af7c745f
Chris Bruce <chris@chrisbruce.com>
2020-04-09 15:41:08 +0200

    wip: hacked running

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,106 @@
 mod loader;
 
-use dotenv::dotenv;
-use loader::Load;
-use std::env;
-
 use clap::{App, Arg};
+use dotenv::dotenv;
+use glob::glob;
+use helium_api::{Account, Client, Hnt};
+use helium_wallet::{
+    cmd_balance, cmd_create, cmd_hotspots, cmd_htlc, cmd_info, cmd_pay, cmd_pay::Payee, cmd_verify,
+    mnemonic, result, traits, wallet,
+};
+use helium_wallet::{result::Result, traits::ReadWrite, wallet::Wallet};
+use loader::Load;
+use std::str::FromStr;
+use std::{env, fs, path::PathBuf, process};
 
 fn main() {
     dotenv().ok();
 
-    let matches = App::new("Helium Load")
-        .version("1.0")
-        .author("Chris Bruce <chris@helium.com>")
-        .about("Provides various options for load testing helium blockchain.")
-        .arg(
-            Arg::with_name("formula")
-                .short("f")
-                .long("formula")
-                .value_name("FORMULA")
-                .help("Which load formula to run: ping | pong | multiply | multiping")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("trigger")
-            .short("t")
-            .long("trigger")
-            .value_name("TRIGGER")
-            .help("Select a trigger type, either on new block or on timed interval. (block | interval)")
-            .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("interval")
-            .short("i")
-            .long("interval")
-            .value_name("INTERVAL")
-            .help("Sets the poll interval in seconds for checking block height or triggering the formula.")
-            .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("accounts")
-            .short("a")
-            .long("accounts")
-            .value_name("NUM_ACCOUNTS")
-            .help("Sets the number of accounts to use for the specified formula.  Defaults to 2.")
-            .takes_value(true),
-        )
-        .get_matches();
+    let api_url = api_url();
+    let password = password();
 
-    let formula = matches.value_of("formula").unwrap_or("ping");
-    println!("Value for formula: {}", formula);
+    create_wallets(25, &password);
+    let wallets = &collect_wallets();
+    let wallet_count: u64 = wallets.len() as u64;
 
-    let trigger = matches.value_of("trigger").unwrap_or("block");
-    println!("Value for trigger: {}", trigger);
+    for payer_wallet in wallets {
+        if let Ok(payer_address) = payer_wallet.address() {
+            let share = get_account_balance(&payer_address) / wallet_count;
+            if share > 0 {
+                let payees: Vec<cmd_pay::Payee> = wallets
+                    .iter()
+                    .filter(|w| w.address().is_ok() && w.address().unwrap() != payer_address)
+                    .map(|w| {
+                        Payee::from_str(&format!("{:?}={:?}", w.address().unwrap(), share)).unwrap()
+                    })
+                    .collect();
 
-    let poll_interval = matches
-        .value_of("interval")
-        .unwrap_or("10")
-        .parse::<u64>()
-        .unwrap();
-    println!("Value for polling interval: {}", poll_interval);
+                let r = cmd_pay::cmd_pay(
+                    api_url.clone(),
+                    payer_wallet,
+                    &password,
+                    payees,
+                    true,
+                    false,
+                );
 
-    let account_num = matches
-        .value_of("accounts")
-        .unwrap_or("10")
-        .parse::<u64>()
-        .unwrap();
-    println!("Value for account number: {}", account_num);
+                println!("Payment result: {:?}", r);
+            }
+        }
+    }
+}
 
-    let api_url = env::var("API_URL").expect("Missing API_URL env var.");
-    let password = env::var("PASSWORD").expect("Missing PASSWORD env var.");
-    let load = Load::new(&api_url, &password);
+fn create_wallets(count: usize, password: &str) {
+    for i in 1..=count {
+        let n = format!("wallet_{}.key", i);
+        let path = PathBuf::from(n);
+        if cmd_create::cmd_basic(password, 1_000, path.clone(), false, None).is_err() {
+            println!("{:?} already exists.", path.display())
+        }
+    }
+}
 
-    let _ = match formula {
-        _ => load.start_pong(poll_interval),
-    };
+/// Find all .key files in dir
+fn collect_wallets() -> Vec<Wallet> {
+    let mut wallets: Vec<Wallet> = vec![];
+
+    for entry in glob("*.key").expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => {
+                println!("Found wallet: {:?}", path.display());
+                let mut reader = fs::File::open(path).unwrap();
+                wallets.push(Wallet::read(&mut reader).unwrap());
+            }
+            Err(e) => println!("{:?}", e),
+        }
+    }
+    wallets
+}
+
+fn fan_out(wallets: &Vec<Wallet>, api_url: &str, password: &str) {
+    for w in wallets {}
+}
+
+fn get_account(address: &str) -> Option<Account> {
+    let client = Client::new_with_base_url(api_url());
+
+    match client.get_account(&address) {
+        Ok(account) => Some(account),
+        _ => None,
+    }
+}
+
+fn get_account_balance(address: &str) -> u64 {
+    match get_account(address) {
+        Some(account) => account.balance,
+        _ => 0,
+    }
+}
+
+fn api_url() -> String {
+    env::var("API_URL").expect("Missing API_URL env var.")
+}
+
+fn password() -> String {
+    env::var("PASSWORD").expect("Missing PASSWORD env var.")
 }
